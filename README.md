4158531bac375d5b2cd50c3e8ef8cc06cbb0028f
JB <198005711+baumjessi@users.noreply.github.com>
2025-09-16 19:43:56 -0500

    Add photos and specials to about.js
    Add three photos and DOM elements pertaining to restaurant
    specials to about.js. Further formatting needed.

--- README.md
+++ README.md
@@ -13,14 +13,36 @@ through JS modules.
 
 ## Media
 Background image:
+
 Font:
+
+About Me Photo 1: https://unsplash.com/photos/a-persons-hands-on-a-plate-of-food-NvBpp2XUQsU
+About Me Photo 2: https://unsplash.com/photos/man-in-green-shirt-sitting-beside-table-R9zuDLne2Bg
+About Me Photo 3: https://unsplash.com/photos/a-person-pouring-a-bottle-of-orange-juice-on-a-pizza-pnJQnnnYlGc
+
+FB Icon: https://www.flaticon.com/free-icon/facebook_3669678?term=facebook&page=1&position=20&origin=search&related_id=3669678
+
+Instagram Icon: https://www.flaticon.com/free-icon/instagram_1384015?term=instagram&page=1&position=6&origin=search&related_id=1384015
+
+Email icon: https://www.flaticon.com/free-icon/email_3178158?term=email&page=1&position=1&origin=search&related_id=3178158
+
+TikTok Icon: https://www.flaticon.com/free-icon/tiktok_3669950?term=tik+tok&page=1&position=9&origin=search&related_id=3669950
+
+Add to Cart Icon: https://www.flaticon.com/free-icon/add-cart_4153710?term=add+to+cart&page=1&position=7&origin=search&related_id=4153710
+
+Order Button Arrow: https://www.flaticon.com/free-icon/fast-forward_724927?term=arrow&page=1&position=4&origin=search&related_id=724927
+
+Header icon: https://www.flaticon.com/free-icon/pizza_161554?term=pizza&page=1&position=72&origin=search&related_id=161554
+
 Pizza Images:
+
 Margherita: https://unsplash.com/photos/pizza-on-brown-wooden-table-PKfz98depf0
 
 Supreme: https://unsplash.com/photos/a-pizza-sitting-on-top-of-a-piece-of-paper-pWqMo3bhv3A?modal=%5B%22Subscribe%22%2C%7B%22sourceAsset%22%3A%5B%22Photos%22%2C%7B%22slug%22%3A%22a-pizza-sitting-on-top-of-a-piece-of-paper-pWqMo3bhv3A%22%7D%5D%7D%5D
 
-Pepperoni:
-Veggie Lover:
+Meat Lover: https://unsplash.com/photos/pizza-with-cheese-and-tomato-_P76trHTWDE
+
+Veggie Lover: https://unsplash.com/photos/a-pizza-sitting-on-top-of-a-wooden-cutting-board-50oL45AbE74;
 
 ## Contact
 If you would like to contact me, you can email me at hellojessib@outlook.com. Thanks for viewing this repo! 
--- src/about.js
+++ src/about.js
@@ -1,13 +1,47 @@
+import Photo1 from "./about-photo-1.jpg";
+import Photo2 from "./about-photo-2.jpg";
+import Photo3 from "./about-photo-3.jpg";
+
 export function loadAbout() {
-    //create title
-    let title = document.createElement("h1");
-    title.textContent = "About Us";
-    //create blurb
-    let blurb = document.createElement("p");
-    blurb.textContent = `Pizza Palace was established in 1983. We have been 
-    serving the Bellevue community proudly for over 40 years. `
-    //append 
-    let content = document.getElementById("content");
-    content.appendChild(title);
-    content.appendChild(blurb);
+  //create title
+  let title = document.createElement("h1");
+  title.textContent = "About Us";
+  title.className = "about-title";
+  //create photos
+  const photo1 = new Image();
+  photo1.src = Photo1;
+  photo1.className = "about-photo";
+  const photo2 = new Image();
+  photo2.src = Photo2;
+  photo2.className = "about-photo";
+  const photo3 = new Image();
+  photo3.src = Photo3;
+  photo3.className = "about-photo";
+  const aboutPhotoDiv = document.createElement("div");
+  aboutPhotoDiv.className = "about-photo-div";
+  aboutPhotoDiv.appendChild(photo1);
+  aboutPhotoDiv.appendChild(photo2);
+  aboutPhotoDiv.appendChild(photo3);
+  //create blurb
+  let blurb = document.createElement("p");
+  blurb.textContent = `Pizza Palace was established in 1983. We have been 
+    serving the Bellevue community proudly for over 40 years. `;
+  //append
+  let content = document.getElementById("content");
+  content.appendChild(title);
+  content.appendChild(aboutPhotoDiv);
+  content.appendChild(blurb);
+
+  //specials
+  let twoForOneDiv = document.createElement("div");
+  twoForOneDiv.className = "specials-div";
+  content.appendChild(twoForOneDiv);
+  let twoForOneHeader = document.createElement("h2");
+  twoForOneHeader.className = "specials-header";
+  twoForOneHeader.innerHTML = "Monday Night <br> 2-4-1 Special";
+  let twoForOneText = document.createElement("p");
+  twoForOneText.innerHTML =
+    "Get one free slice of cheese pizza and one domestic draft beer for every slice and beer you order! Available every Monday from 4PM until close.";
+  twoForOneDiv.appendChild(twoForOneHeader);
+  twoForOneDiv.appendChild(twoForOneText);
 }
